ae62cfee6fc706fdf38fb29ddc8edb1b1f076493
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-10-02 10:45:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whatsapp_api_whapi',
+  webDir: 'www'
+};
+
+export default config;
